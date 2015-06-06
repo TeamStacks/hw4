9ec5f29cda27e0ddbddd8fd9f43c26d11743a655
Angie <angienguyen629@gmail.com>
2015-06-05 19:01:45 -0700

    fixing after database consolidation (no more inventory)

--- public/js/query.js
+++ public/js/query.js
@@ -8,42 +8,37 @@ function parseURLParams() {
 }
 
 function populateTable(metalFrom) {
-    var query = new Parse.Query(Parse.Object.extend("Inventory"));
-    var info = {};
+    var query = new Parse.Query(Parse.Object.extend("Coin"));
+    query.equalTo("ownedBy", Parse.User.current());
 
-    query.get(Parse.User.current().id, {
+    query.find({
     	success: function(results) {
     		for (var i = 0; i < results.length; i++) {
+
     			var curr = results[i]; // all coins in user's inventory
-    			if (coin.get("metal") == metalFrom) {
-    				addToTable();
+    			if (curr.get("metal") == metalFrom) {
+    				    if (curr.get("imageURL") == null) {
+                            var col1 = '<td class="stack_img_col"><div class="coin_mini"></div></td>';
+                        }
+                        else {
+                            var col1 = '<td class="stack_img_col"><img class="img_mini" src="'+metalId.imageURL+'"></td>';
+                        }
+                        var col2 = '<td>' + curr.get("type") + '</td>';
+                        var col3 = '<td>' + curr.get("quantity") + '</td>';
+                        var col4 = '<td>' + curr.get("weightPerUnit") + '</td>';
+                        var col5 = '<td>' + curr.get("metalPercentage") + '</td>';
+                        var col6 = '<td>' + curr.get("value").toFixed(4) + '</td>';
+
+                        $('#inventory > tbody')
+                            .append($('<tr></tr>').attr('onclick', 'inspectCoin(this)')
+                                .attr('id', curr.id).append(col1, col2, col3, col4, col5, col6));
     			}
     				
     		}
     		
     	},
-    	error: function (inventory, error){
-    		alert("You do not have any coins added.");
+    	error: function (error){
+    		alert("You do not have any coins added." + error);
     	}
     });
-
-    
-}
-
-function addToTable(metalId, invenId, coinId) {
-    if (metalId.imageURL == null) {
-        var col1 = '<td class="stack_img_col"><div class="coin_mini"></div></td>';
-    }
-    else {
-        var col1 = '<td class="stack_img_col"><img class="img_mini" src="'+metalId.imageURL+'"></td>';
-    }
-    var col2 = '<td>' + metalId.coinType + '</td>';
-    var col3 = '<td>' + inventories[invenId].quantity + '</td>';
-    var col4 = '<td>' + metalId.weightPerUnit + '</td>';
-    var col5 = '<td>' + metalId.metalPercentage + '</td>';
-    var col6 = '<td>' + inventories[invenId].totalValue.toFixed(4) + '</td>';
-
-    $('#inventory > tbody')
-        .append($('<tr></tr>').attr('onclick', 'inspectCoin(this)')
-            .attr('id', coinId).append(col1, col2, col3, col4, col5, col6));
 }

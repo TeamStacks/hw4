6b168b562764f648bec0ed437b42c77acafb72dd
Angie <angienguyen629@gmail.com>
2015-06-05 18:47:28 -0700

    wire5 adds

--- public/js/query.js
+++ public/js/query.js
@@ -1,31 +1,3 @@
-function addItem() {
-	console.log("Adding an Item");
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://api.parse.com/1/classes/test", true);
-	xhr.setRequestHeader("X-Parse-Application-Id", "m8lsJLtBtcOGqT0h9RmcWRIpxpiwyb3Yq6HqrCk2");
-	xhr.setRequestHeader("X-Parse-REST-API-Key", "JrtCwMABQRvOOtixhzLAZwk46nhxnUlJBIuxx1NZ");
-	xhr.setRequestHeader("Content-Type", "application/json");
-
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
-	    var result = JSON.parse(xhr.responseText);
-	    if (result.objectId) {
-	      console.log("saved an object with id: " + result.objectId);
-	      
-	    }
-	  }
-	}
-	  
-	var data = JSON.stringify({ message: "Hello world!" });
-	xhr.send(data);
-
-	//document.getElementById("test").innerHTML = "HELLO";
-
-	console.log(xhr.statusText);
-
-	// maybe do a refresh here if you want or something
-}
-
 function parseURLParams() {
     var params = {};
     window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(ms, key,value) {
@@ -33,4 +5,45 @@ function parseURLParams() {
         params[key] = value;
     });
     return params;
+}
+
+function populateTable(metalFrom) {
+    var query = new Parse.Query(Parse.Object.extend("Inventory"));
+    var info = {};
+
+    query.get(Parse.User.current().id, {
+    	success: function(results) {
+    		for (var i = 0; i < results.length; i++) {
+    			var curr = results[i]; // all coins in user's inventory
+    			if (coin.get("metal") == metalFrom) {
+    				addToTable();
+    			}
+    				
+    		}
+    		
+    	},
+    	error: function (inventory, error){
+    		alert("You do not have any coins added.");
+    	}
+    });
+
+    
+}
+
+function addToTable(metalId, invenId, coinId) {
+    if (metalId.imageURL == null) {
+        var col1 = '<td class="stack_img_col"><div class="coin_mini"></div></td>';
+    }
+    else {
+        var col1 = '<td class="stack_img_col"><img class="img_mini" src="'+metalId.imageURL+'"></td>';
+    }
+    var col2 = '<td>' + metalId.coinType + '</td>';
+    var col3 = '<td>' + inventories[invenId].quantity + '</td>';
+    var col4 = '<td>' + metalId.weightPerUnit + '</td>';
+    var col5 = '<td>' + metalId.metalPercentage + '</td>';
+    var col6 = '<td>' + inventories[invenId].totalValue.toFixed(4) + '</td>';
+
+    $('#inventory > tbody')
+        .append($('<tr></tr>').attr('onclick', 'inspectCoin(this)')
+            .attr('id', coinId).append(col1, col2, col3, col4, col5, col6));
 }

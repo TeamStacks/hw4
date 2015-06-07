4a145acea9c11bc308652566adc9a9a0a668468f
Wai Ho Leung <whleung@live.com>
2015-06-07 01:32:07 -0700

    Refactored populateTable.

--- public/js/query.js
+++ public/js/query.js
@@ -35,38 +35,24 @@ function queryOwnedCoins(metal, success) {
 }
 
 function populateTable(metalFrom) {
-    var query = new Parse.Query(Parse.Object.extend("Coin"));
-    query.equalTo("ownedBy", Parse.User.current());
+    queryOwnedCoins(metalFrom, function (results) {
+        var curr = results[i]; // all coins in user's inventory
 
-    query.find({
-    	success: function(results) {
-    		for (var i = 0; i < results.length; i++) {
+        if (curr.get("imageURL") == null) {
+            var col1 = '<td class="stack_img_col"><div class="coin_mini"></div></td>';
+        } else {
+            var col1 = '<td class="stack_img_col"><img class="img_mini" src="' + curr.get("imageURL") + '"></td>';
+        }
 
-    			var curr = results[i]; // all coins in user's inventory
-    			if (curr.get("metal") == metalFrom) {
-                    console.log(results.length);
-    				    if (curr.get("imageURL") == null) {
-                            var col1 = '<td class="stack_img_col"><div class="coin_mini"></div></td>';
-                        }
-                        else {
-                            var col1 = '<td class="stack_img_col"><img class="img_mini" src="'+curr.get("imageURL")+'"></td>';
-                        }
-                        var col2 = '<td>' + curr.get("type") + '</td>';
-                        var col3 = '<td>' + curr.get("quantity") + '</td>';
-                        var col4 = '<td>' + curr.get("weightPerUnit") + '</td>';
-                        var col5 = '<td>' + curr.get("metalPercentage") + '</td>';
-                        var col6 = '<td>' + curr.get("value").toFixed(4) + '</td>';
-console.log(results.length);
-                        $('#inventory > tbody')
-                            .append($('<tr></tr>').attr('onclick', 'inspectCoin(this)')
-                                .attr('id', curr.id).append(col1, col2, col3, col4, col5, col6));
-    			}
-    				
-    		}
-    		
-    	},
-    	error: function (error){
-    		alert("You do not have any coins added." + error);
-    	}
+        var col2 = '<td>' + curr.get("type") + '</td>';
+        var col3 = '<td>' + curr.get("quantity") + '</td>';
+        var col4 = '<td>' + curr.get("weightPerUnit") + '</td>';
+        var col5 = '<td>' + curr.get("metalPercentage") + '</td>';
+        var col6 = '<td>' + curr.get("value").toFixed(4) + '</td>';
+
+        $('#inventory > tbody')
+            .append($('<tr></tr>').attr('onclick', 'inspectCoin(this)')
+                .attr('id', curr.id).append(col1, col2, col3, col4, col5, col6));
+
     });
 }
